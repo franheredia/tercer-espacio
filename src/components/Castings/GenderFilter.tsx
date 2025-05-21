30491ade41cdfc04d1409f45a8121a7587ad55d5
Franco Heredia <franheredia.ov@gmail.com>
2025-05-21 17:42:23 -0300

    added: wpp-btn for castings

--- src/components/Castings/GenderFilter.tsx
+++ src/components/Castings/GenderFilter.tsx
@@ -28,28 +28,42 @@ const GenderFilter: React.FC<GenderFilterProps> = ({ selectedGender, onGenderCha
     { id: 'nonbinary', label: 'Roles no binarios' }
   ];
 
+  const wppMessage = encodeURIComponent(
+    `Hola Tercer Espacio! Les mando un casting para sumar a su página:\n_(y soy consciente de que sólo se publican castings que paguen al menos viáticos)_\n*Título de la propuesta:* \n*Fisic to rol:* (opcional)\n*Descripción de la propuesta:*\n*Teléfono de contacto:*\n*Info necesaria para aplicar:* Fotos de cara y cuerpo completo\n*Link con más información:* (opcional)\n*Remuneración:* Cubre viáticos y remuneración\n*Fecha de vencimiento de la publicación:* `
+  );
+
   return (
     <div className="gender-filter">
-      <div className="gender-filter__group">
-        {genderOptions.map(option => (
-          <div key={option.id} className="gender-filter__option">
-            <input
-              type="radio"
-              id={option.id}
-              name="role-filter"
-              value={option.id}
-              checked={selectedGender === option.id}
-              onChange={(e) => onGenderChange(e.target.value as Gender)}
-              className="gender-filter__input"
-            />
-            <label 
-              htmlFor={option.id}
-              className="gender-filter__label"
-            >
-              {option.label}
-            </label>
-          </div>
-        ))}
+      <div className="gender-filter__container">
+        <div className="gender-filter__group">
+          {genderOptions.map(option => (
+            <div key={option.id} className="gender-filter__option">
+              <input
+                type="radio"
+                id={option.id}
+                name="role-filter"
+                value={option.id}
+                checked={selectedGender === option.id}
+                onChange={(e) => onGenderChange(e.target.value as Gender)}
+                className="gender-filter__input"
+              />
+              <label
+                htmlFor={option.id}
+                className="gender-filter__label"
+              >
+                {option.label}
+              </label>
+            </div>
+          ))}
+        </div>
+        <a
+          className="gender-filter__whatsapp-button"
+          href={`https://wa.me/+5493518119701?text=${wppMessage}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Agregar un casting
+        </a>
       </div>
     </div>
   );
