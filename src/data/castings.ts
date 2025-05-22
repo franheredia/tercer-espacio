46d5e23c7d4bdc335c3a3106c6bb835672482167
Franco Heredia <franheredia.ov@gmail.com>
2025-05-22 13:45:52 -0300

    added: new castings data

--- src/data/castings.ts
+++ src/data/castings.ts
@@ -3,14 +3,34 @@ import { createDate } from '@/snippets';
 
 export const completeCastings: Casting[] = [
     {
-        title: "Niña - Corto Universitario",
+        title: "Rol Fem - Mujer Adulta",
         gender: "female",
-        fisic: "Aparentar 11 años, Cabello castaño lacio, Ojos marrones",
-        description: "Viáticos y Remuneración",
-        phone: "5493541695958",
+        fisic: "Aparentar 50-60 años",
+        description: "Remunerado. Rodaje el sábado 24 de mayo",
+        phone: "542664483324",
         requiredInfo: "fotos de cuerpo completo y de rostro",
         infoUrl: "",
-        endDate: createDate("21-05-2025")
+        endDate: createDate("23-05-2025")
+    },
+    {
+        title: "Rol Fem - Mujer Joven",
+        gender: "female",
+        fisic: "Aparentar 20-25 años",
+        description: "Remunerado. Rodaje el sábado 24 de mayo",
+        phone: "542664483324",
+        requiredInfo: "fotos de cuerpo completo y de rostro",
+        infoUrl: "",
+        endDate: createDate("23-05-2025")
+    },
+    {
+        title: "Personas Chilenas o Mexicanas",
+        gender: "any",
+        fisic: "Importante que mantengan su tonada y hablen frente a cámara",
+        description: "Es para grabar una serie de videos educativos. Remunerado",
+        phone: "5493512394808",
+        requiredInfo: "un audio presentándome",
+        infoUrl: "",
+        endDate: createDate("31-05-2025")
     },
     {
         title: "Rol Masc. - Corto Universitario",
