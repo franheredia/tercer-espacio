7672e73ab94ef67f555e60fdeb764eea8b3158ad
Franco Heredia <franheredia.ov@gmail.com>
2025-05-27 09:58:41 -0300

    updated: castings list

--- src/data/castings.ts
+++ src/data/castings.ts
@@ -3,36 +3,6 @@ import { createDate } from '@/snippets';
 
 export const completeCastings: Casting[] = [
     {
-        title: "Masc. Extra - Grabación de escena",
-        gender: "male",
-        fisic: "Alguien que aparente 20-25 años, y alguien que aparente 40-50 años.",
-        description: "Buscamos extras para filmar el 24/05. Se cubren costos de transporte urbano y desayuno.",
-        phone: "542664483324",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("23-05-2025")
-    },
-    {
-        title: "Fem. Extra - Grabación de escena",
-        gender: "female",
-        fisic: "Alguien que aparente 20-25 años.",
-        description: "Buscamos extras para filmar el 24/05. Se cubren costos de transporte urbano y desayuno.",
-        phone: "542664483324",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("23-05-2025")
-    },
-    {
-        title: "Casting para obra de Teatro",
-        gender: "any",
-        fisic: "Entre 25-60 años.",
-        description: "Para sumarse a una obra teatral de Drama Psicológico con mucho trabajo corporal.",
-        phone: "5493515329000",
-        requiredInfo: "foto de mi rostro y un video con una breve de presentación",
-        infoUrl: "",
-        endDate: createDate("23-05-2025")
-    },
-    {
         title: "Personas Chilenas o Mexicanas",
         gender: "any",
         fisic: "Importante que mantengan su tonada y hablen frente a cámara.",
@@ -41,55 +11,5 @@ export const completeCastings: Casting[] = [
         requiredInfo: "un audio presentándome",
         infoUrl: "",
         endDate: createDate("31-05-2025")
-    },
-    {
-        title: "Rol Masc. - Corto Universitario",
-        gender: "male",
-        fisic: "Aparentar 25-30 años, tez clara, cabello largo castaño lacio, con barba.",
-        description: "Remunerado.",
-        phone: "5493512064750",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("25-05-2025")
-    },
-    {
-        title: "Rol Masc. - Corto Universitario",
-        gender: "male",
-        fisic: "Aparentar 21-25 años, tez clara, cabello corto castaño claro, sin barba.",
-        description: "Remunerado.",
-        phone: "5493512064750",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("25-05-2025")
-    },
-    {
-        title: "Rol Fem. - Corto Universitario",
-        gender: "female",
-        fisic: "Aparentar 21-25 años, tez clara, cabello corto castaño oscuro.",
-        description: "Remunerado.",
-        phone: "5493512064750",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("25-05-2025")
-    },
-    {
-        title: "Rol Fem. - Corto Universitario",
-        gender: "female",
-        fisic: "Aparentar 21-25 años, tez clara, cabello largo mechas rubias.",
-        description: "Remunerado.",
-        phone: "5493512064750",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("25-05-2025")
-    },
-    {
-        title: "Rol Fem. - Corto Universitario",
-        gender: "female",
-        fisic: "Aparentar 21-25 años, tez clara, cabello largo castaño oscuro.",
-        description: "Remunerado.",
-        phone: "5493512064750",
-        requiredInfo: "fotos de mi rostro y cuerpo completo",
-        infoUrl: "",
-        endDate: createDate("25-05-2025")
     }
 ]; 
